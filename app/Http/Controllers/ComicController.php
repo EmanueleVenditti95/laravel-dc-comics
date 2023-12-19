4c7515f48cb0521eead5e3f069f78d067c093d31
EmanueleVenditti95 <vendittiema@gmail.com>
2023-12-19 14:00:16 +0100

    Refactory - store CRUD

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -31,19 +31,21 @@ class ComicController extends Controller
     public function store(StoreComicRequest $request)
     {
         $data = $request->all();
-        $newComic = new Comic();
+        // $newComic = new Comic();
 
-        $newComic->title = $data['title'];
-        $newComic->description = $data['description'];
-        $newComic->thumb = $data['thumb'];
-        $newComic->price = $data['price'];
-        $newComic->series = $data['series'];
-        $newComic->sale_date = $data['sale_date'];
-        $newComic->type = $data['type'];
+        // $newComic->title = $data['title'];
+        // $newComic->description = $data['description'];
+        // $newComic->thumb = $data['thumb'];
+        // $newComic->price = $data['price'];
+        // $newComic->series = $data['series'];
+        // $newComic->sale_date = $data['sale_date'];
+        // $newComic->type = $data['type'];
+        // $newComic->save();
 
-        $newComic->save();
+        // metodo per aggiungere tutto insieme, definendo la variabile $fillable nel Model
+        $newComic = Comic::create($data);
 
-        return redirect()->route('comics.index',$newComic->id);
+        return redirect()->route('comics.show',$newComic->id);
     }
 
     /**
