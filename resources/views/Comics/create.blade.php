894b2b07e14c57acfa7be44d3c5e34812c5caa6f
EmanueleVenditti95 <vendittiema@gmail.com>
2023-12-19 10:15:41 +0100

    Fix - authorization

--- resources/views/Comics/create.blade.php
+++ resources/views/Comics/create.blade.php
@@ -13,33 +13,33 @@
 
             <div class="form-group">
               <label for="title">Titolo</label>
-              <input type="text" class="form-control" name="title" placeholder="Inserisci titolo">
+              <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci titolo">
             </div>
             <div class="form-group">
               <label for="description">Descrizione</label>
-              <input type="text" class="form-control" name="description" placeholder="Inserisci descrizione">
+              <input type="text" class="form-control" name="description" id="description" placeholder="Inserisci descrizione">
             </div>
             <div class="form-group">
                 <label for="thumb">Immagine</label>
-                <input type="title" class="form-control" name="thumb" placeholder="Inserisci link immagine">
+                <input type="title" class="form-control" name="thumb" id="thumb" placeholder="Inserisci link immagine">
               </div>
               <div class="form-group">
                 <label for="price">Prezzo</label>
-                <input type="text" class="form-control" name="price" placeholder="Inserisci prezzo">
+                <input type="text" class="form-control" name="price" id="price" placeholder="Inserisci prezzo">
               </div>
               <div class="form-group">
                 <label for="series">Serie</label>
-                <input type="text" class="form-control" name="series" placeholder="Inserisci serie">
+                <input type="text" class="form-control" name="series" id="series" placeholder="Inserisci serie">
               </div>
               <div class="form-group">
                 <label for="sale_date">Data di vendita</label>
-                <input type="text" class="form-control" name="sale_date" placeholder="Inserisci data di vendita">
+                <input type="text" class="form-control" name="sale_date" id="sale_date" placeholder="YYYY/MM/DD">
               </div>
               <div class="form-group">
                 <label for="type">Genere</label>
-                <input type="text" class="form-control" name="type" placeholder="Inserisci genere">
+                <input type="text" class="form-control" name="type" id="type" placeholder="Inserisci genere">
               </div>
-            <button type="submit" class="btn btn-primary mt-3">Inserisci</button>
+            <input type="submit" class="btn btn-primary mt-3" value="Inserisci">
           </form>
     </div>
 @endsection
